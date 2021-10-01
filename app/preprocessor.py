016911cec0f69378f9984bb0efe7e019721dba51
Diana Corona <diana.corona.g@gmail.com>
2021-09-30 19:06:47 -0500

    fix: change preprocesor to be lighter

--- app/preprocessor.py
+++ app/preprocessor.py
@@ -1,26 +1,7 @@
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
-from nltk import word_tokenize  
-from nltk import sent_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import SnowballStemmer
-from nltk.stem import WordNetLemmatizer
+import re
 
 def preprocessor(text):
-    all_words = word_tokenize(text)
-    #lowercase text
-    lower_case_words = [w.lower() for w in all_words]
-    words = [word for word in lower_case_words if word.isalpha()]
-    #remove unwanted words
-    remove_words_list = set(stopwords.words('english'))
-    filtered_words = [w for w in words if not w in remove_words_list]
-    filtered_words = [w for w in filtered_words if not w in "br"]
-    #lemmatenize words
-    lemmatized_words = [WordNetLemmatizer().lemmatize(w) for w in filtered_words]
-    #steam words
-    porter = SnowballStemmer('english')
-    stemmed_words = [porter.stem(word) for word in lemmatized_words]
-    final_text = ' '.join([str(word) for word in stemmed_words ])
-    return final_text
+    text = re.sub('<[^>]*>', '', text) # Effectively removes HTML markup tags
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
+    text = re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', '')
+    return text
